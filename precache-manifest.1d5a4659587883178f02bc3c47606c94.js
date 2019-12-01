3270d71a7b0062e1935f0926a1c53e1156eda95e
Flavio <flavio2201@gmail.com>
2019-12-01 15:54:18 -0600

    Updates

---
+++ precache-manifest.1d5a4659587883178f02bc3c47606c94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2380f9c9ca392064cd66abf4133bdaa7",
+    "url": "/c3site/index.html"
+  },
+  {
+    "revision": "c40ce874ea0c652a8339",
+    "url": "/c3site/static/css/main.e0ee829e.chunk.css"
+  },
+  {
+    "revision": "918d49d9f487623706f2",
+    "url": "/c3site/static/js/2.3e5ee7af.chunk.js"
+  },
+  {
+    "revision": "c40ce874ea0c652a8339",
+    "url": "/c3site/static/js/main.4f753657.chunk.js"
+  },
+  {
+    "revision": "95721c1909b7fc8b7c41",
+    "url": "/c3site/static/js/runtime-main.a69e521f.js"
+  }
+]);
